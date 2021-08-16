779f297fe5c8db550980c20c7a9f0f0c0721ab12
hsuandy <88942302+hsuandy@users.noreply.github.com>
2021-08-15 23:43:19 -0700

    Update battleship.py
    Added a title screen, game over screen, retry option, and refactored some functions.

--- battleship.py
+++ battleship.py
@@ -1,14 +1,30 @@
 import os
 import pandas as pd
 from random import randint, choice
+from art import tprint
 from rich import print as rprint
 
-# Resize and clear terminal
+# ASCII art found at https://www.asciiart.eu/vehicles/navy, credit to Matthew Bace
+ship = '''[red]
+                                     |__
+                                     |\/
+                                     ---
+                                     / | [
+                              !      | |||
+                            _/|     _/|-++'
+                        +  +--|    |--|--|_ |-
+                     { /|__|  |/\__|  |--- |||__/
+                    +---------------___[}-_===_.'____
+                ____`-' ||___-{]_| _[}-  |     |_[___\==--
+ __..._____--==/___]_|__|_____________________________[___\==--____,------'-.
+|                                                                          /
+ \_________________________________________________________________________|
+[/]'''
+
+# Resize terminal
 def initialize_terminal(height = 80, width = 35):
     cmd = f"printf '\e[8;{width};{height}t'"
-    clr = "clear"
     os.system(cmd)
-    os.system(clr)
 
 # Generate a grid map for the game by creating a dataframe
 def create_grid(row, col, sea_icon):
@@ -83,14 +99,14 @@ def initialize_hitpoints(fleet):
     return sum([size for size in fleet.values()])
 
 def check_game_state(player_life, opponent_life):
+    state = 0
     if player_life == 0 and opponent_life == 0:
-        return True, "[bold yellow]Mutual destruction, it's a tie![/]"
+        state = 1 # Draw
     elif player_life == 0:
-        return True, "[bold red1]Opponent wins![/]"
+        state = 2 # Player loses
     elif opponent_life == 0:
-        return True, f"[bold green1]{player_name} wins![/]"
-    else:
-        return False, ''
+        state = 3 # Player wins
+    return state
 
 def ai_target_selection(target_queue, player_fleet_map):
     if len(target_queue) > 0:
@@ -116,6 +132,23 @@ def ai_targeting_update(target, p_row_max, p_col_max, targeted_list):
         target_queue.append((target[0], target[1] - 1))
     return target_queue
 
+def game_over_screen(game_state):
+    os.system('clear')
+    if game_state == 1:
+        tprint("DRAW")
+        rprint("[bold yellow]Mutual destruction, both fleets have been sunk![/]", '\n')
+    elif game_state == 2:
+        tprint("YOU LOSE")
+        rprint("[bold red1]Your opponent has sunk your fleet![/]", '\n')
+    elif game_state == 3:
+        tprint("YOU WIN")
+        rprint(f"[bold green1]{player_name} has triumphed![/]", '\n')
+    choice = input("Enter Y to play again, or any other key to quit: ")
+    if choice.lower() == 'y':
+        return True
+    else:
+        return False
+
 # Draw UI
 def draw_ui():
     print('\n')
@@ -124,78 +157,88 @@ def draw_ui():
     rprint(player_fleet_map,'\n\n')
     rprint('[bold red]Opponent[/]')
     rprint(f'Damage Points Remaining: [orange1]{opponent_life}[/]', '\n')
-    #rprint(opponent_fleet_map, '\n') # Print map with opponent fleet coordinates
+    rprint(opponent_fleet_map, '\n') # Print map with opponent fleet coordinates
     rprint(opponent_display, '\n')
     rprint(output_message, '\n')
 
-# Attempt to resize terminal (may not work with all terminal types)
-initialize_terminal()
+retry = True
 
-# Initialize game data
-player_name = input('Enter your name, Admiral: ') or 'Player'
-player_fleet = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine':3, 'Destroyer': 2}
-#player_fleet = {'Cruiser': 3} # Test fleet data
-player_life = initialize_hitpoints(player_fleet)
-opponent_fleet = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine':3, 'Destroyer': 2}
-#opponent_fleet = {'Cruiser': 3} # Test fleet data
-opponent_life = initialize_hitpoints(opponent_fleet)
-sea_icon = '·'
-hit_icon = '*'
-miss_icon = '+'
-output_message = ''
-game_over = False
-player_display = create_grid(10, 10, sea_icon)
-player_fleet_map = create_fleet_map(player_display, player_fleet)
-opponent_display = create_grid(10, 10, sea_icon)
-opponent_fleet_map = create_fleet_map(opponent_display, opponent_fleet)
-p_row_max = player_fleet_map.shape[0]
-p_col_max = player_fleet_map.shape[1]
-target_queue = []
-targeted_list = []
+# External game loop for enabling a retry option when game ends
+while retry:
+    # Attempt to resize terminal (may not work with all terminal types)
+    initialize_terminal()
+    os.system('clear')
 
-# Main game loop
-while game_over == False:
-    draw_ui()
+    # Initialize game data
+    tprint("BATTLESHIP")
+    rprint(ship, '\n')
+    print("Welcome to the Naval Battle Simulator.", '\n')
+    player_name = input('Enter your name: ') or 'Player'
+    player_fleet = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine':3, 'Destroyer': 2}
+    #player_fleet = {'Cruiser': 3} # Test fleet data
+    player_life = initialize_hitpoints(player_fleet)
+    opponent_fleet = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine':3, 'Destroyer': 2}
+    #opponent_fleet = {'Cruiser': 3} # Test fleet data
+    opponent_life = initialize_hitpoints(opponent_fleet)
+    sea_icon = '·'
+    hit_icon = '*'
+    miss_icon = '+'
+    output_message = ''
+    player_display = create_grid(10, 10, sea_icon)
+    player_fleet_map = create_fleet_map(player_display, player_fleet)
+    opponent_display = create_grid(10, 10, sea_icon)
+    opponent_fleet_map = create_fleet_map(opponent_display, opponent_fleet)
+    p_row_max = player_fleet_map.shape[0]
+    p_col_max = player_fleet_map.shape[1]
+    o_row_max = opponent_fleet_map.shape[0]
+    o_col_max = opponent_fleet_map.shape[1]
+    target_queue = []
+    targeted_list = []
 
-    # Player turn
-    try:
-        opp_row = int(input('Enter target row value (number): '))
-        opp_col = ord(input('Enter target column value (letter): ').lower()) - 97
-        opp_target = opp_row, opp_col
-        opp_result = update_fleet_map(opponent_fleet_map, opponent_display, opp_target, sea_icon, hit_icon, miss_icon)
-        if opp_result == True:
-            output_message = "It's a [green1]hit[/]!"
-            opponent_life -= 1
-        else:
-            output_message = "It's a [yellow]miss[/]!"
-    except ValueError:
-        output_message = "[red]Please enter a number for the row value and a letter for the column value.[/]"
-    except IndexError:
-        output_message = "[red]Please enter a valid value.[/]"
-    except KeyError:
-        output_message = "[yellow]This coordinate has already been targeted.[/]"
+    # Internal game loop for running the game
+    while True:
+        draw_ui()
 
-    draw_ui()
+        # Player turn
+        try:
+            opp_row = int(input(f'Enter target row value (0 - {o_row_max - 1}): '))
+            opp_col = ord(input(f'Enter target column value (A - {chr(ord("@") + o_col_max)}): ').lower()) - 97
+            opp_target = opp_row, opp_col
+            opp_result = update_fleet_map(opponent_fleet_map, opponent_display, opp_target, sea_icon, hit_icon, miss_icon)
+            if opp_result == True:
+                output_message = "It's a [green1]hit[/]!"
+                opponent_life -= 1
+            else:
+                output_message = "It's a [yellow]miss[/]!"
+        except ValueError:
+            output_message = "[red]TARGETING ERROR!\nValue entered is invalid.[/]"
+        except TypeError:
+            output_message = "[red]TARGETING ERROR!\nValue entered is invalid.[/]"
+        except IndexError:
+            output_message = "[red]TARGETING ERROR!\nCoordinate entered is out of range.[/]"
+        except KeyError:
+            output_message = "[yellow]This coordinate has already been targeted.[/]"
 
-    input("Opponent's Turn (Press ENTER to continue)")
+        draw_ui()
+        input("Opponent's Turn (Press ENTER to continue)")
 
-    # Opponent turn
-    player_target = ai_target_selection(target_queue, player_fleet_map)
-    while player_target in targeted_list:
+        # Opponent turn
         player_target = ai_target_selection(target_queue, player_fleet_map)
-    targeted_list.append(player_target)
-    player_result = update_fleet_map(player_fleet_map, player_display, player_target, sea_icon, hit_icon, miss_icon)
-    if player_result == True:
-        output_message = "You've been [red1]hit[/]!"
-        player_life -= 1
-        target_queue += ai_targeting_update(player_target, p_row_max, p_col_max, targeted_list)
-    else:
-        output_message = "Opponent [yellow]missed[/]!"
+        while player_target in targeted_list:
+            player_target = ai_target_selection(target_queue, player_fleet_map)
+        targeted_list.append(player_target)
+        player_result = update_fleet_map(player_fleet_map, player_display, player_target, sea_icon, hit_icon, miss_icon)
+        if player_result == True:
+            output_message = "You've been [red1]hit[/]!"
+            player_life -= 1
+            target_queue += ai_targeting_update(player_target, p_row_max, p_col_max, targeted_list)
+        else:
+            output_message = "Opponent [yellow]missed[/]!"
 
-    # Check if there is a winner
-    game_state, winner_msg = check_game_state(player_life, opponent_life)
-    if game_state:
-        game_over = True
-        output_message = winner_msg
+        # Check if there is a winner
+        game_state = check_game_state(player_life, opponent_life)
+        if game_state > 0:
+            retry = game_over_screen(game_state)
+            break
 
-    draw_ui()
+        draw_ui()
